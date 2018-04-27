5f75df3de311f6619d378e60875d572d88ce39dc
LB519911 <liubiaozh@outlook.com>
2018-04-27 09:04:34 +0800

    1.更新测试类的测试类和资源文件。内置Tomcat无法启动请使用本地Tomcat。 2.在/webapp/WEB-INF/js下添加了jq.js。

--- src/main/java/cn/liubiao/controller/IndexController.java
+++ src/main/java/cn/liubiao/controller/IndexController.java
@@ -2,11 +2,19 @@ package cn.liubiao.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
     @RequestMapping("/")
     public String getIndex(){
         return "index";
+    }
+
+    @RequestMapping("/string")
+    @ResponseBody
+    public String getDtring(@RequestParam("string") String string){
+        return string;
     }
 }
---
+++ src/test/java/cn/liubiao/test/Test.java
@@ -0,0 +1,10 @@
+package cn.liubiao.test;
+
+public class Test {
+
+    @org.junit.Test
+    public void test(){
+
+    }
+
+}
